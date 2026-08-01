eb5c4e2725ba699878eb43e702796274bda33e28
Jawad Ahmad <jdmgl2238@gmail.com>
2026-08-02 00:03:00 +0500

    Time: 11 ms (82.56%), Space: 24.1 MB (9.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,20 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         
-        list1 = []
 
+        results = defaultdict(list)
         for x in strs:
-            list1.append(str(sorted(x)))
-        
-        set1 = set(list1)
-        final = []
-        results = []
+            count = [0] * 26
+            for y in x:
+                count[ord(y) - ord('a')] += 1
+            results[tuple(count)].append(x)
 
-        for word in set1:
-            for y in range(len(list1)):
-                if word == list1[y]: 
-                    results.append(strs[y])
-            final.append(results)
-            results = []
-         
-        return final
+        return list(results.values())
+                
+
