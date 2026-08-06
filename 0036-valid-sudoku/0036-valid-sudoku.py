e63f859dc8ec55f4920a0ef6fbbdc7a9f61e2801
Jawad Ahmad <jdmgl2238@gmail.com>
2026-08-06 22:19:13 +0500

    Time: 6 ms (31.69%), Space: 19.4 MB (9.63%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,19 +1,31 @@
 class Solution:
+
+    def get_bit(self, no, index):
+        return (no & (1 << index)) != 0
+
     def isValidSudoku(self, board: List[List[str]]) -> bool:
 
-        rows = defaultdict(set)
-        columns = defaultdict(set)
-        squares = defaultdict(set)
+        rows = [0] * 9
+        columns = [0] * 9
+        squares = [0] * 9
 
         for row in range(9):
             for col in range(9):
+
                 if board[row][col] == ".":
                     continue
-                if (board[row][col] in rows[row]) or (board[row][col] in columns[col]) or (board[row][col] in squares[row // 3 , col // 3]):
+
+                val = int(board[row][col]) - 1
+
+                square = (row // 3) * 3 + (col // 3)
+
+                if (self.get_bit(rows[row], val)
+                    or self.get_bit(columns[col], val)
+                    or self.get_bit(squares[square], val)):
                     return False
-                 
-                columns[col].add(board[row][col])
-                rows[row].add(board[row][col])
-                squares[row // 3 , col // 3].add(board[row][col])
-        
+
+                rows[row] |= (1 << val)
+                columns[col] |= (1 << val)
+                squares[square] |= (1 << val)
+
         return True
